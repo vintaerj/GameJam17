0bfac7f0c9964760f1966f96c9dab4e54f64cb91
vintaerj <jojovintaer@hotmail.fr>
2018-09-19 21:43:47 +0200

    tracked files ressources/

---
+++ GameJam17/GameJam17/Gameplay/VehiculeJoueur.cs
@@ -0,0 +1,14 @@
+using GameJam17.Personnage;
+
+namespace GameJam17.Gameplay
+{
+    public class VehiculeJoueur
+    {
+
+        private Grid map;
+        private string nom;
+        private Membre[] equipe;
+        private int pv;
+
+    }
+}
---
+++ GameJam17/GameJam17/Personnage/Membre.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace GameJam17.Personnage
+{
+    public class Membre
+    {
+
+        private Dictionary<string, int> competences;
+        private string nom;
+    }
+}
